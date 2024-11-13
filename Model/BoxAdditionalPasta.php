af110cd07f7fcce247207fca27e600c94680ec4c
Salah Gad <salahgad26@gmail.com>
2024-11-13 21:53:44 +0200

    implemented decorator for box
    Co-Authored-By: Youssef Walid <105242821+Obsessi0n9@users.noreply.github.com>

--- Model/BoxAdditionalPasta.php
+++ Model/BoxAdditionalPasta.php
@@ -4,8 +4,31 @@ require_once 'BoxDecorator.php';
 class BoxAdditionalPasta extends BoxDecorator {
     private $numPackets;
 
-    public function getContentDetails(): array {
-        // Return contents with additional pasta
+    /**
+     * @return mixed
+     */
+    public function getNumPackets()
+    {
+        return $this->numPackets;
+    }
+
+    /**
+     * @param mixed $numPackets
+     */
+    public function setNumPackets($numPackets): void
+    {
+        $this->numPackets = $numPackets;
+    }
+
+
+    public function getContentDetails(): string {
+        return $this->ref->getContentDetails()." , additional pasta packets ".$this->numPackets;
+    }
+
+    public function calculateCost(): float
+    {
+        return ($this->numPackets * 10) + $this->ref->calculateCost(); //10 is price of 1 packet of pasta
+
     }
 }
 ?>
--- Model/BoxAdditionalRice.php
+++ Model/BoxAdditionalRice.php
@@ -2,10 +2,32 @@
 require_once 'BoxDecorator.php';
 
 class BoxAdditionalRice extends BoxDecorator {
-    private $weight;
+    protected $weight;
 
-    public function getContentDetails(): array {
-        // Return contents with additional rice
+    /**
+     * @return mixed
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param mixed $weight
+     */
+    public function setWeight($weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    public function getContentDetails(): string {
+        return $this->ref->getContentDetails()." , additional rice of weight ".$this->weight;
+    }
+
+    public function calculateCost(): float
+    {
+        return ($this->weight * 30) + $this->ref->calculateCost(); //30 is price of 1 kg of rice
+
     }
 }
 ?>
